dee12c70bc2243736416104649362417edfbe553
Vaibhav Varshney <vaibhavvarshney@Vaibhavs-MacBook-Air.local>
2023-07-16 01:55:35 +0530

    snackbar added

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import {
   FlatList,
   Pressable,
   TouchableOpacity,
+  StatusBar,
 } from 'react-native';
 import {initialValue} from './Constant';
 import ListItem from './components/ListItem/ListItem';
@@ -24,55 +25,59 @@ const App = () => {
   };
 
   const checkWinner = (data: string[]) => {
-    let flag = false;
+    let flag = '';
     if (data[0] !== 'nothing' && data[0] === data[1] && data[1] === data[2]) {
-      flag = true;
+      flag = data[0];
     } else if (
       data[3] !== 'nothing' &&
       data[3] === data[4] &&
       data[4] === data[5]
     ) {
-      flag = true;
+      flag = data[3];
     } else if (
       data[6] !== 'nothing' &&
       data[6] === data[7] &&
       data[7] === data[8]
     ) {
-      flag = true;
+      flag = data[6];
     } else if (
       data[0] !== 'nothing' &&
       data[0] === data[3] &&
       data[3] === data[6]
     ) {
-      flag = true;
+      flag = data[0];
     } else if (
       data[1] !== 'nothing' &&
       data[1] === data[4] &&
       data[4] === data[7]
     ) {
-      flag = true;
+      flag = data[1];
     } else if (
       data[2] !== 'nothing' &&
       data[2] === data[5] &&
       data[5] === data[8]
     ) {
-      flag = true;
+      flag = data[2];
     } else if (
       data[0] !== 'nothing' &&
       data[0] === data[4] &&
       data[4] === data[8]
     ) {
-      flag = true;
+      flag = data[0];
     } else if (
       data[2] !== 'nothing' &&
       data[2] === data[4] &&
       data[4] === data[6]
     ) {
-      flag = true;
+      flag = data[2];
     }
 
     if (flag) {
-      setWinner(data[0] === 'cross' ? 'X' : '0');
+      setWinner(flag === 'cross' ? 'X' : '0');
+      Snackbar.show({
+        text: `Player ${flag === 'cross' ? 'X' : '0'} won!!! 🚀`,
+        backgroundColor: '#7cb305',
+      });
     }
   };
 
@@ -94,17 +99,18 @@ const App = () => {
 
   return (
     <SafeAreaView>
+      <StatusBar backgroundColor="#515151" />
       <View
         style={[
           styles.messageBox,
           // eslint-disable-next-line react-native/no-inline-styles
           {
-            backgroundColor: winner ? '#5aff28' : '#fadb14',
+            backgroundColor: winner ? '#7cb305' : '#fadb14',
           },
         ]}>
         <Text style={styles.messageTxt}>
           {winner
-            ? `Player ${winner} win`
+            ? `Player ${winner} win 🚀`
             : `Player ${step % 2 === 0 ? 'X' : '0'}'s Turn`}
         </Text>
       </View>
